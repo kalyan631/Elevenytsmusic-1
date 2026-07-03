58e070b147612194a16dfd956d94646e824cf2f5
🖤ः⎯꯭̽࠾࡙ࠬ𝆬иσв࡙ιтα𝆺𝅥┽•࡙ꭙ𝆬𝆬𝆬𝆬𝂇࠾ࠬ❤ <kalyanpari67@gmail.com>
2026-07-03 21:28:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ class Config:
         self.SESSION3: str = getenv("STRING_SESSION3", "")
 
         # Support Links
-        self.SUPPORT_CHANNEL: str = getenv("SUPPORT_CHANNEL", "https://t.me/elevenytsmusic")
+        self.SUPPORT_CHANNEL: str = getenv("SUPPORT_CHANNEL", "https://t.me/")
         self.SUPPORT_CHAT: str = getenv("SUPPORT_CHAT", "https://t.me/+rTXlUr25H1YwOThl")
 
         # Excluded Chats
